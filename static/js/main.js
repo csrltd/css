0ef82622769267864940e73c01f885d769eb125e
Charmant12 <sgcharmant@compstaffing.com>
2023-02-24 15:33:38 +0200

    added mobile version on home an about us page

--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,25 @@ toggleBtns.forEach(function (toggleBtn) {
 
 
 
+// NAVBAR
+const nav = document.getElementById('nav')
+const navToggle = document.querySelector('.nav-toggle')
+const closeBtn = document.querySelector('.close-btn')
+const menuItems = document.querySelector('.menu-items')
+
+navToggle.addEventListener('click', () => {
+  nav.classList.add('fixed-nav')
+  menuItems.style.display = 'block'
+})
+
+closeBtn.addEventListener('click', () => {
+  nav.classList.remove('fixed-nav')
+  menuItems.style.display = 'none'
+})
+
+
+
+
 
 
 
